30932499539858d21b4383e682ee3952edd0274f
jangur <sanju11223344@gmail.com>
2024-10-10 11:09:54 +0530

    saw

---
+++ nodejs/index.js
@@ -0,0 +1,13 @@
+// index.js
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(8000, () => {
+  console.log('Server running on port 8000');
+});
